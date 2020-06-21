4895b0ddc7373a2700e49eb24d08deedefa29216
Dmitry Falin <falin.d.14@mail.ru>
2020-06-21 17:06:37 +0300

    added creating and filling SQL scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE meals_seq RESTART WITH 1;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,12 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories) VALUES
+    ('2020-01-30 10:00', 'Завтрак', 500),
+    ('2020-01-30 13:00', 'Обед', 1000),
+    ('2020-01-30 20:00', 'Ужин', 500),
+    ('2020-01-31 00:00', 'Еда на граничное значение', 100),
+    ('2020-01-31 10:00', 'Завтрак', 500),
+    ('2020-01-31 13:00', 'Обед', 1000),
+    ('2020-01-31 20:00', 'Ужин', 410)
