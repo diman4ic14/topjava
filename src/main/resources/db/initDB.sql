45a2a6b8404f5214bb84d570064f3a1339788d74
Dmitry Falin <falin.d.14@mail.ru>
2020-06-22 15:00:36 +0300

    added field 'user_id' in Table meals implemented JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -30,5 +30,7 @@ CREATE TABLE meals
     id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     date_time   TIMESTAMP       NOT NULL,
     description VARCHAR         NOT NULL,
-    calories    INTEGER         NOT NULL
+    calories    INTEGER         NOT NULL,
+    user_id     INTEGER         NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
